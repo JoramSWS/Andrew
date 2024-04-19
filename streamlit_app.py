e616d2f75a6b803a37dd532da09cc088fd0dfbeb
Joram Schwartz <161503181+JoramSWS@users.noreply.github.com>
2024-04-19 13:39:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,22 @@ if "openai_model" not in st.session_state:
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-        
+
 name = st.radio(
     "Select a name:",
     ["Chris", "Andrew"],
-    )
+)
 
 prompt_template = {
     "Chris": "Gruff tone with a fondness for wordplay and portmanteaus,",
     "Andrew": "From Texas, boisterous, angry tone,",
- }[name]
+}[name]
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-    
+
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
             model=st.session_state["openai_model"],
@@ -33,6 +33,7 @@ if prompt := st.chat_input("What is up?"):
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
+            prompt=prompt_template,
             stream=True,
         )
         response = st.write_stream(stream)
