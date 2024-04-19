f8bdc74fa0c3a472e34d517328c62fc2c163974f
Joram Schwartz <161503181+JoramSWS@users.noreply.github.com>
2024-04-19 15:58:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ def page_1():
 def page_2():
     PROMPT_TEMPLATE = """
     
-    Answer the question using portmanteaus or clever wordplay, but you are also tired and impatient: {question}
+    Answer the question in a clever or sarchastic tone, using portmanteaus or clever wordplay: {question}
 
     """
 
